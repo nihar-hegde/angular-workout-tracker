ca73a4709b0f400ae8643765313a6f47b1ba6576
nihar-hegde <niharhegde163@gmail.com>
2024-07-11 16:58:18 +0530

    Initial commit , add tailwindcss, add input form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputFormComponent } from './components/input-form/input-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InputFormComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'workout-tracker';
