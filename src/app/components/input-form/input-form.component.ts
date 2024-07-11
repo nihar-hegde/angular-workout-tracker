256d7d44d83a34d21ed880fd500801e730d3489a
nihar-hegde <niharhegde163@gmail.com>
2024-07-11 21:22:24 +0530

    1. added material angular 2. setup a basic input component

--- src/app/components/input-form/input-form.component.ts
+++ src/app/components/input-form/input-form.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-input-form',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+  ],
   templateUrl: './input-form.component.html',
-  styleUrl: './input-form.component.css'
+  styleUrls: ['./input-form.component.css'],
 })
 export class InputFormComponent {
+  userName: string = '';
+  selectedWorkout: string = '';
+  workoutMinutes: number | null = null;
 
+  workoutOptions: string[] = [
+    'Running',
+    'Cycling',
+    'Swimming',
+    'Yoga',
+    'Weightlifting',
+  ];
 }
